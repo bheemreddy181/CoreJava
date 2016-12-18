f08eb7ec6e56584246099193461f143c62eaee29
nagachandra nelavelli <nagachandra nelavelli>
2016-12-18 11:11:27 -0500

    added boolean operators and precedence and associativity related files

---
+++ src/com/manoharacademy/operators/PrecedenceAndAssociativity.java
@@ -0,0 +1,10 @@
+package com.manoharacademy.operators;
+
+public class PrecedenceAndAssociativity {
+    public static void main(String[] args) {
+        //Precedence
+        System.out.printf("1 + 2 * 3 = %d\n" , (1 + 2) * 3); //3 * 3 = 9  // 1 + 6 = 7
+        //Associativity
+        System.out.printf("8 / 4 / 2 = %d\n" , 8 / (4 / 2)); // 2/2 =1    // 8/2 =4   
+    }
+}
