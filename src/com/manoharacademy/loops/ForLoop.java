e41837a528d6e2ddaab4b11bfc63ac5951fa8cd8
nagachandra nelavelli <nagachandra nelavelli>
2016-12-23 12:01:03 -0500

    added loop constructs

---
+++ src/com/manoharacademy/loops/ForLoop.java
@@ -0,0 +1,22 @@
+package com.manoharacademy.loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        
+//        int i = 0; // Initiation
+//        while(i <= 10){ //Validate a boolean expression
+//            System.out.println("Current value of i is : " + i);
+//            i++; //Update loop controller value
+//        }
+        
+        for(int i = 0 ;  i <= 10 ; i++ ){
+           System.out.println("Current value of i is : " + i); 
+        }
+        
+//        1) initializaiton stetements will be evaluated
+//                2) expression is evaluated, if it is true then execute task
+//                        3) execute task
+//                                4) update variable
+            
+    }
+}
